a4f46aeec6301ed4e07c7a27836d83516aaa61e4
Administrator <cdpjhzw@foxmail.com>
2020-01-22 15:03:48 +0800

    gateway

--- zw_gateway/src/main/java/com/gateway/config/ExampleConfiguration.java
+++ zw_gateway/src/main/java/com/gateway/config/ExampleConfiguration.java
@@ -15,37 +15,37 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class ExampleConfiguration {
 
-    @Bean
-    @Order(-1)
-    public GlobalFilter a() {
-        return (exchange, chain) -> {
-            log.info("first pre filter");
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                log.info("third post filter");
-            }));
-        };
-    }
-
-    @Bean
-    @Order(0)
-    public GlobalFilter b() {
-        return (exchange, chain) -> {
-            log.info("second pre filter");
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                log.info("second post filter");
-            }));
-        };
-    }
-
-    @Bean
-    @Order(1)
-    public GlobalFilter c() {
-        return (exchange, chain) -> {
-            log.info("third pre filter");
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                log.info("first post filter");
-            }));
-        };
-    }
+//    @Bean
+//    @Order(-1)
+//    public GlobalFilter a() {
+//        return (exchange, chain) -> {
+//            log.info("first pre filter");
+//            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//                log.info("third post filter");
+//            }));
+//        };
+//    }
+//
+//    @Bean
+//    @Order(0)
+//    public GlobalFilter b() {
+//        return (exchange, chain) -> {
+//            log.info("second pre filter");
+//            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//                log.info("second post filter");
+//            }));
+//        };
+//    }
+//
+//    @Bean
+//    @Order(1)
+//    public GlobalFilter c() {
+//        return (exchange, chain) -> {
+//            log.info("third pre filter");
+//            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//                log.info("first post filter");
+//            }));
+//        };
+//    }
 
 }
