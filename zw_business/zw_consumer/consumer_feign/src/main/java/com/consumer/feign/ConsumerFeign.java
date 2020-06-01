b61d1bde590364b78c88204ed0a41a371d110aeb
hzw <huangzhongwang@xtoneict.com>
2020-06-01 15:36:40 +0800

    feign

--- zw_business/zw_consumer/consumer_feign/src/main/java/com/consumer/feign/ConsumerFeign.java
+++ zw_business/zw_consumer/consumer_feign/src/main/java/com/consumer/feign/ConsumerFeign.java
@@ -2,9 +2,8 @@ package com.consumer.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "consumer", path = "/consumerFeign",fallbackFactory = void.class)
-public class ConsumerFeign {
-
+@FeignClient(name = "consumer", path = "/consumerFeign", fallbackFactory = void.class)
+public interface ConsumerFeign {
 
 
 }
